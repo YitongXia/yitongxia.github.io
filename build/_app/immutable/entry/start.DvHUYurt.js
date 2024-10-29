bcc3091978dfb372d8bfb0a0f59dce263797830f
Yitong Xia <yitong.xia@kuleuven.be>
2024-10-29 22:47:33 +0100

    fix:hover effect

---
+++ build/_app/immutable/entry/start.DvHUYurt.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D6t7GCNE.js";export{t as start};
---
+++ src/routes/Map/+page.svelte
@@ -0,0 +1,43 @@
+<!-- For best results, preview this in either the full-width or full-screen preview modes. -->
+
+<script>
+    import { base } from '$app/paths';
+    const navigateToAbout = () => {
+    window.location.href = `${base}/Aboutme`;
+    };
+
+    const navigateToHome = () => {
+    window.location.href = `${base}/`;
+    };
+    const navigateToPublication = () => {
+    window.location.href = `${base}/Publication`;
+    };
+
+    const navigateToActivity = () => {
+    window.location.href = `${base}/Activity`;
+    };
+
+    const navigateToLifeMap = () => {
+    window.location.href = `${base}/LifeMap`;
+    };  
+
+
+  </script>
+  
+  <div class="grid grid-rows-[auto_1fr_auto]">
+      <!-- Header -->
+      <header class= "p-4">
+        <div class="flex space-x-4">
+            <button on:click={navigateToHome} class="hover-effect btn font-mono">Home</button>
+            <button on:click={navigateToAbout} class="hover-effect btn font-mono">
+              <a href="{base}/Aboutme">About me</a>
+            </button>
+            <button on:click={navigateToPublication} class="hover-effect btn font-mono">
+                <a href="{base}/Publication">Publication</a>
+            </button>
+            <button on:click={navigateToActivity} class="hover-effect btn font-mono font-bold">Activity</button>
+            <button on:click={navigateToLifeMap} class="hover-effect btn font-mono">Life map</button>
+        </div>
+    </header>
+    </div>
+    
